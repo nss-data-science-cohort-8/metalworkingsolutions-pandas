0687fa936df9d22c6d01b45951512442219d6db7
Andrew Richard <acrichard07@gmail.com>
2025-03-14 13:55:34 -0700

    made connection in R, grouped by generated revenue to get top 20 customers in '23 and '24

--- ar_seasonal_trends.sql
+++ ar_seasonal_trends.sql
@@ -1,2 +1,2 @@
-
 --  How has the volume of work changed for each customer over time? Are there any seasonal patterns? How have the number of estimated hours per customer changed over time? Estimated hours are in the jmo_estimated_production_hours columns of the job_operations_2023/job_operations_2024 tables.  
+
